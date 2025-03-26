7458eecab316fb0ea2e02bd87018f5390e96460c
abz0 <143391418+abz0@users.noreply.github.com>
2025-03-26 13:46:38 +1100

    destory_bullet_upon_collision

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -12,6 +12,11 @@ public class Projectile : Weapon
         transform.position += -transform.right * speed * Time.deltaTime;
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
     public float GetProjectileSpeed() { return speed; }
 
     public void SetProjectileSpeed(float speed) { this.speed = speed; }
