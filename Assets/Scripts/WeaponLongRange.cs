a1a41d48cf5eb6b2d7b7ef808c7877d51e3bd583
abz0 <143391418+abz0@users.noreply.github.com>
2025-03-26 13:36:53 +1100

    bullet_is_delayed

--- Assets/Scripts/WeaponLongRange.cs
+++ Assets/Scripts/WeaponLongRange.cs
@@ -6,25 +6,49 @@ public class WeaponLongRange : Weapon
 {
     [Header("Projectile Details")]
     [SerializeField] private GameObject projectile;
-    [SerializeField] private Transform launchOffLocation;
+    [SerializeField] private Transform launchOffLocation; //where the projectile will appear
+    [SerializeField] private float launchDelay; //the time taken for the projectile to appear
     [SerializeField] private float speed = 1f;
+
+    private float timeDelay = 0f;
+    private bool triggerAttack = false;
 
     void Start()
     {
         Projectile pComponent = projectile.GetComponent<Projectile>();
         pComponent.SetDamage(GetDamage());
         pComponent.SetProjectileSpeed(speed);
+
+        ResetProjectileDelay();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (CanAttackEnemy(KeyCode.Return))
         {
             AttackEnemy();
+        }
+        else
+        {
+            ResetProjectileDelay();
         }
     }
 
     public override void AttackEnemy()
+    {
+        if (timeDelay > launchDelay)
+        {
+            SpawnProjectile();
+
+            timeDelay = 0f;
+        }
+        else
+        {
+            timeDelay += Time.deltaTime;
+        }
+    }
+
+    private void SpawnProjectile()
     {
         //Note: the newProjectile gameobject is a sample in place of the actual bullet
         GameObject newProjectile = Instantiate(projectile, launchOffLocation.position, transform.rotation);
@@ -33,5 +57,24 @@ public class WeaponLongRange : Weapon
         Projectile newPComponent = newProjectile.GetComponent<Projectile>();
         Debug.Log("Projectile Damage: " + newPComponent.GetDamage());
         Debug.Log("Projectile Speed: " + newPComponent.GetProjectileSpeed());
+    }
+
+    private bool CanAttackEnemy(KeyCode keycode)
+    {
+        if (Input.GetKeyDown(keycode)) 
+        { 
+            triggerAttack = true; 
+        }
+        else if (Input.GetKeyUp(keycode)) 
+        { 
+            triggerAttack = false; 
+        }
+
+        return triggerAttack;
+    }
+
+    private void ResetProjectileDelay()
+    {
+        timeDelay = launchDelay + 1f;
     }
 }
