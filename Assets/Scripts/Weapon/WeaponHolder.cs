4f51bfd4979249cf6a66fdc5b2b2d5f921e27204
abz0 <143391418+abz0@users.noreply.github.com>
2025-03-27 22:23:34 +1100

    find_weapon_by_index

--- Assets/Scripts/Weapon/WeaponHolder.cs
+++ Assets/Scripts/Weapon/WeaponHolder.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WeaponHolder : MonoBehaviour
 {
-    [SerializeField] private WeaponType weaponType;
+    [SerializeField] private WeaponType selectedWeapon;
+
+    private List<GameObject> weapons = new List<GameObject>();
+    private int weaponIndex = -1;
+    private int previousWeaponIndex = -1;
+
+    void Awake()
+    {
+        GetAvailableWeapons();
+    }
 
     void Start()
     {
@@ -12,6 +22,57 @@ public class WeaponHolder : MonoBehaviour
     }
 
     void Update()
+    {
+        WeaponFacePointer();
+    }
+
+    private void GetAvailableWeapons()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.GetComponent<Weapon>())
+            {
+                //Debug.Log(child.name);
+                weapons.Add(child.gameObject);
+            }
+        }
+    }
+
+    private bool FindWeapon(WeaponType type)
+    {
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            if (i != previousWeaponIndex 
+                && weapons[i].GetComponent<Weapon>().GetWeaponType() == type)
+            {
+                previousWeaponIndex = weaponIndex;
+                weaponIndex = i;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void SelectWeapon()
+    {
+        if (FindWeapon(selectedWeapon))
+        {
+            weapons[weaponIndex].SetActive(true);
+        }
+
+        Debug.Log("yes");
+    }
+
+    private Vector2 FacePointerPosition()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        return (mousePosition - (Vector2)transform.position).normalized;
+    }
+
+    private void WeaponFacePointer()
     {
         Vector2 direction = FacePointerPosition();
 
@@ -22,29 +83,7 @@ public class WeaponHolder : MonoBehaviour
         {
             scale.y *= -1;
         }
+
         transform.localScale = scale;
-    }
-
-    private void SelectWeapon()
-    {
-        foreach (Transform weapon in transform)
-        {
-            Debug.Log(weapon.name);
-            if (weapon.GetComponent<Weapon>().GetWeaponType() == weaponType)
-            {
-                weapon.gameObject.SetActive(true);
-            }
-            else
-            {
-                weapon.gameObject.SetActive(false);
-            }
-        }
-    }
-
-    private Vector2 FacePointerPosition()
-    {
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        return (mousePosition - (Vector2)transform.position).normalized;
     }
 }
