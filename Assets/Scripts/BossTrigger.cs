0d651b417430aeb5691f4ad70f75e822302a6e4b
SoulSlurper97 <143369302+SoulSlurper97@users.noreply.github.com>
2025-04-13 15:23:52 +1000

    Changed boss trigger

--- Assets/Scripts/BossTrigger.cs
+++ Assets/Scripts/BossTrigger.cs
@@ -27,7 +27,7 @@ public class BossTrigger : MonoBehaviour
         {
             Instantiate(boss);
             hasSpawnedBoss = true;
-            mainCamera.orthographicSize += 2f;
+            mainCamera.orthographicSize = 5f;
             //gameObject.SetActive(false);
         }
     }
