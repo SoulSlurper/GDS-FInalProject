3c0c7ba2d65ff8c9605ef1af3ca6af800b5b654a
Ryan Chan <ryan.w.chan@student.uts.edu.au>
2025-05-04 19:28:41 +1000

    Fixed bugs MissingComponentException: There is no 'Rigidbody2D' attached to the "Ground" game object...
    MissingComponentException: There is no 'Rigidbody2D' attached to the "Ground" game object, but a script is trying to access it.
    You probably need to add a Rigidbody2D to the game object "Ground". Or your script needs to check if the component is attached before using it.
    UnityEngine.Object+MarshalledUnityObject.TryThrowEditorNullExceptionObject (UnityEngine.Object unityObj, System.String parameterName) (at <6b66e7caaeb045048a0fbc11f111e6fa>:0)
    UnityEngine.Bindings.ThrowHelper.ThrowNullReferenceException (System.Object obj) (at <6b66e7caaeb045048a0fbc11f111e6fa>:0)
    UnityEngine.Rigidbody2D.get_velocity () (at <c03889a4b4984101a827b6573211d699>:0)
    WaterWaves2D.OnTriggerExit2D (UnityEngine.Collider2D other) (at Assets/WaterWaves2D/Script/WaterWaves2D.cs:372)

--- Assets/WaterWaves2D/Script/WaterWaves2D.cs
+++ Assets/WaterWaves2D/Script/WaterWaves2D.cs
@@ -361,23 +361,30 @@ public class WaterWaves2D:MonoBehaviour{
 		));
 		PlayEnterSound();
 	}
-	
-	private void OnTriggerExit2D(Collider2D other){
-		Vector2 closest=new Vector2();
-		#if UNIT2019_0_1_OR_NEWER
-			closest=other.ClosestPoint(other.transform.position)
-		#else
-			closest=other.bounds.ClosestPoint(other.transform.position);
-		#endif
-		Impact(
-			closest,
-			(Vector2)other.bounds.extents,
-			other.GetComponent<Rigidbody2D>().velocity
-		);
-		PlayExitSound();
-	}
 
-	private void ScheduleImpact(Vector2 position,Vector2 size,Vector2 force,float delay){
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Vector2 closest = new Vector2();
+		#if UNIT2019_0_1_OR_NEWER
+			closest = other.ClosestPoint(other.transform.position);
+		#else
+			closest = other.bounds.ClosestPoint(other.transform.position);
+		#endif
+
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        Vector2 velocity = rb != null ? rb.velocity : Vector2.zero;
+
+        Impact(
+            closest,
+            (Vector2)other.bounds.extents,
+            velocity
+        );
+
+        PlayExitSound();
+    }
+
+
+    private void ScheduleImpact(Vector2 position,Vector2 size,Vector2 force,float delay){
 		StartCoroutine(DelayedImpact(position,size,force,delay));
 	}
 	
